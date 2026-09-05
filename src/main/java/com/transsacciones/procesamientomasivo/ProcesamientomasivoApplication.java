9810bd754551114dfe91d7f0aa81d9645fe6a4c7
FJSolares <sorfec153@gmail.com>
2026-09-04 18:24:45 -0600

    commit inicial

---
+++ src/main/java/com/transsacciones/procesamientomasivo/ProcesamientomasivoApplication.java
@@ -0,0 +1,13 @@
+package com.transsacciones.procesamientomasivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcesamientomasivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcesamientomasivoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/transsacciones/procesamientomasivo/ProcesamientomasivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transsacciones.procesamientomasivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcesamientomasivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
